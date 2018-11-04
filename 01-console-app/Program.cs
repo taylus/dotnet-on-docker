d7e955d6039a2e13ba043ee3fd1c5b6d67f379ac
taylus <brandon@legacy.codes>
2018-11-04 13:45:07 -0600

    Initial commit

---
+++ 01-console-app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
